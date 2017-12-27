79a1e62925693720a5fde136346af1409d380949
Illya Moskvin <imoskvin@artic.edu>
2017-12-27 14:34:35 -0600

    Add ResourceSerializer
    Not a fan of this. It might be malfunctioning for included items.
    This deserves another look after everything's stable.

---
+++ src/ResourceSerializer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aic\Hub\Core;
+
+use League\Fractal\Serializer\DataArraySerializer;
+
+class ResourceSerializer extends DataArraySerializer
+{
+
+   /**
+     * Serialize a collection.
+     *
+     * @param string $resourceKey
+     * @param array  $data
+     *
+     * @return array
+     */
+    public function collection($resourceKey, array $data)
+    {
+
+        if ($resourceKey === false) {
+            return $data;
+        }
+
+        return array($resourceKey ?: 'data' => $data);
+
+    }
+
+    /**
+     * Serialize an item.
+     *
+     * @param string $resourceKey
+     * @param array  $data
+     *
+     * @return array
+     */
+    public function item($resourceKey, array $data)
+    {
+
+        if ($resourceKey === false) {
+            return $data;
+        }
+
+        return array($resourceKey ?: 'data' => $data);
+
+    }
+
+}
