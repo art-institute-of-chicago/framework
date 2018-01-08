21e4fcb344c95063a3c0d6682a20253ad1cc3da5
Illya Moskvin <imoskvin@artic.edu>
2018-01-08 00:31:09 -0600

    Add default HttpKernel
    Needs more thought. Meant to reduce boilerplate.
    Modify `bootstrap/app.php` to use this in dependents.

---
+++ src/HttpKernel.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Aic\Hub\Foundation;
+
+use Illuminate\Foundation\Http\Kernel as BaseKernel;
+
+class HttpKernel extends BaseKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * These middleware are run during every request to your application.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \Illuminate\Foundation\Http\Middleware\TrimStrings::class,
+        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        \Aic\Hub\Foundation\Middleware\CORS::class,
+        \Aic\Hub\Foundation\Middleware\ETagMiddleware::class,
+    ];
+
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+        ],
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * These middleware may be assigned to groups or used individually.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+    ];
+}
