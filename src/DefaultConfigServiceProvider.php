ae5db9e9112381d6a15115924d43a21234d91fb9
Illya Moskvin <imoskvin@artic.edu>
2018-01-21 14:47:40 -0600

    Add publishable config/app.php
    It seems that we must include an `app.php` with both `providers`
    and `aliases` in the app itself. We can omit all other keys: they
    will be loaded from the default `app.php` in the foundation.
    
    I've added a publishable version of `app.php` to the foundation.
    You can publish it to your app with the following command:
    
    php artisan vendor:publish --provider="Aic\Hub\Foundation\DefaultConfigServiceProvider" --force
    
    Alternatively, you can call `php artisan vendor:publish --force`
    and select the provider via the offered menu.

--- src/DefaultConfigServiceProvider.php
+++ src/DefaultConfigServiceProvider.php
@@ -13,27 +13,55 @@ class DefaultConfigServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    private $defaultConfigPath = __DIR__.'/../config';
+    private $defaultConfigPath = __DIR__.'/../config/default';
 
     /**
-     * Bootstrap any application services.
+     * Path to directory where published config files are stored.
+     *
+     * @var string
+     */
+    private $publishConfigPath = __DIR__.'/../config/publish';
+
+    /**
+     * This publishes the required `config/app.php` file to your app, which
+     * contains the only settings that must live in the app, instead of the
+     * foundation: `providers` and `aliases`. Run this to publish it:
+     *
+     * php artisan vendor:publish --provider="Aic\Hub\Foundation\DefaultConfigServiceProvider" --force
+     *
+     * Note the quotes. This will overwrite your existing `config/app.php`.
      *
      * @return void
      */
     public function boot()
     {
-        //
+
+        $this->publishes([
+            $this->publishConfigPath . '/app.php' => config_path('app.php'),
+        ]);
+
     }
 
     /**
-     * Register any application services.
+     * This provider merges default config files from the foundation with the
+     * ones in your app. This enables us to omit them from our apps, reducing
+     * code duplication across microservices.
+     *
+     * If you need to customize configs, try to use `.env` settings whenever
+     * possible. If that's not possible, you can copy default config files to
+     * your app, and omit any first-level keys you don't want to customize.
+     *
+     * There is one limitation to this approach: you must always include the
+     * `config/app.php` file in your app, with the `providers` and `aliases`
+     * keys. See the `boot` method here for more info.
+     *
+     * @link https://laravel.com/docs/5.5/packages#resources
      *
      * @return void
      */
     public function register()
     {
 
-        // List all PHP files in ../config
         $files = glob( $this->defaultConfigPath . '/*.php' );
 
         foreach( $files as $file )
