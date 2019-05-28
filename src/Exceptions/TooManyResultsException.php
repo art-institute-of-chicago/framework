a62001a1579a3b08b011b7db53224833aaa87c87
nikhiltri <ntrivedi@artic.edu>
2019-05-28 17:11:15 -0500

    Add Too Many Results Exception [WEB-937]

---
+++ src/Exceptions/TooManyResultsException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Aic\Hub\Foundation\Exceptions;
+
+use Illuminate\Http\Response;
+
+class TooManyResultsException extends AbstractException
+{
+
+    protected $message = 'Invalid number of results';
+
+    protected $detail = 'You have requested too many results. Please refine your parameters.';
+
+    protected $code = Response::HTTP_FORBIDDEN;
+
+}
