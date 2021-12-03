69b1ba42cf3eb7156911fbe2d7783eb1cd03b126
Illya Moskvin <imoskvin@artic.edu>
2021-12-03 16:51:41 -0600

    Add BlueprintServiceProvider [API-212, API-226]

--- src/Providers/FoundationServiceProvider.php
+++ src/Providers/FoundationServiceProvider.php
@@ -10,6 +10,7 @@ class FoundationServiceProvider extends AggregateServiceProvider
      * These providers will be auto-loaded, so only put harmless stuff here.
      */
     protected $providers = [
+        BlueprintServiceProvider::class,
         CommandServiceProvider::class,
     ];
 }
