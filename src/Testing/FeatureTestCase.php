1ab62c69d7d39ede8b3b1c3313c95a65724539de
Illya Moskvin <imoskvin@artic.edu>
2022-03-25 12:28:38 -0500

    Add `FeatureTestCase` [API-268]

---
+++ src/Testing/FeatureTestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Aic\Hub\Foundation\Testing;
+
+use Tests\CreatesApplication;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class FeatureTestCase extends BaseTestCase
+{
+    /**
+     * It is reasonable to expect that this trait must exist. See docs.
+     *
+     * @link https://laravel.com/docs/8.x/testing#the-creates-application-trait
+     */
+    use CreatesApplication;
+
+    /**
+     * @link https://laravel.com/docs/8.x/database-testing#resetting-the-database-after-each-test
+     */
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        foreach (class_uses_recursive($this) as $trait) {
+            if (method_exists($this, $method = 'setUp' . class_basename($trait))) {
+                $this->{$method}();
+            }
+        }
+
+        parent::setUp();
+    }
+}
--- src/Testing/ResourceTestCase.php
+++ src/Testing/ResourceTestCase.php
@@ -2,27 +2,13 @@
 
 namespace Aic\Hub\Foundation\Testing;
 
-use Tests\CreatesApplication;
 use Aic\Hub\Foundation\Concerns\HasAbstractProperties;
 use Aic\Hub\Foundation\Testing\Concerns\HasFields;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Aic\Hub\Foundation\Testing\FeatureTestCase as BaseTestCase;
 
 abstract class ResourceTestCase extends BaseTestCase
 {
     use HasAbstractProperties;
-
-    /**
-     * It is reasonable to expect that this trait must exist. See docs.
-     *
-     * @link https://laravel.com/docs/8.x/testing#the-creates-application-trait
-     */
-    use CreatesApplication;
-
-    /**
-     * @link https://laravel.com/docs/8.x/database-testing#resetting-the-database-after-each-test
-     */
-    use RefreshDatabase;
 
     /**
      * Define `protected function fields()` in child classes.
@@ -38,13 +24,6 @@ abstract class ResourceTestCase extends BaseTestCase
     {
         $this->checkProperty('model');
 
-        foreach (class_uses_recursive($this) as $trait) {
-            if (method_exists($this, $method = 'setUp' . class_basename($trait))) {
-                $this->{$method}();
-            }
-        }
-
         parent::setUp();
     }
-
 }
