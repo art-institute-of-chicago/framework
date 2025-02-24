65c1aeebb787bedbd3449dd3a46108295c8df97b
nikhiltri <ntrivedi@artic.edu>
2025-02-24 15:07:24 -0600

    Remove CreatesApplication test trait [WEB-2953]

--- src/Testing/FeatureTestCase.php
+++ src/Testing/FeatureTestCase.php
@@ -2,20 +2,12 @@
 
 namespace Aic\Hub\Foundation\Testing;
 
-use Tests\CreatesApplication;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\RefreshDatabaseState;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class FeatureTestCase extends BaseTestCase
 {
-    /**
-     * It is reasonable to expect that this trait must exist. See docs.
-     *
-     * @link https://laravel.com/docs/8.x/testing#the-creates-application-trait
-     */
-    use CreatesApplication;
-
     /**
      * @link https://laravel.com/docs/8.x/database-testing#resetting-the-database-after-each-test
      */
